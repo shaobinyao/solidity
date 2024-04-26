3ed8a37c6458241d1b88d0b10401dc4c1d9dfdae
mark <296567770@qq.com>
2024-04-26 11:06:21 +0800

    valuetype_address

--- 02_ValueType.sol
+++ 02_ValueType.sol
@@ -16,5 +16,24 @@ contract ValueType{
     // != 
     bool public _bool5 = _bool != _bool4;
 
+    // 整型
+    int public a = -1; //证书，包含负数
+    uint public b = 1; //正整数
+    uint256 public c = 20240404; //256位正整数
+    // + - * / %(取余) **(幂)
+    uint public d = 3**2;
+    uint public e = 13%3;
+    bool public _numberbool = d < e;
+
+    //地址类型
+    //有2种地址类型
+    //普通地址：address，存储一个20字节的值（以太坊地址的大小）
+    //payable address：比普通地址多了transfer和send两个成员方法，用于接收转账
+    address public _address1 = 0x4535F0f36Ce48C296Cc0935808fdE9B6443d4800;
+    address payable _address2 = payable(_address1); //可以转账和查余额
+    uint public _balance = _address2.balance;
+
+    
+
     
 }
